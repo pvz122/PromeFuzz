04a664baf27167a58dd649c17081f941712f01a7
pvz122 <pvz122@126.com>
2025-10-15 17:05:57 +0800

    fix: incompatible abseil version when building re2 drivers

--- database/re2/install_deps.sh
+++ database/re2/install_deps.sh
@@ -3,6 +3,8 @@
 # sudo apt install libabsl-dev
 git clone https://github.com/abseil/abseil-cpp.git
 cd abseil-cpp
+# This version of abseil is compatible with re2 c84a140
+git checkout 735c86164a69141f33ccfcb20ecf1b9254be32a7
 cp ../disable_abseil_debug.patch .
 git apply disable_abseil_debug.patch
 mkdir build && cd build
